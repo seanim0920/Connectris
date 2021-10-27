adf4c7d3bfd9ad4ba7aa6f6fbeb986a6b1f3b8a7
sean <seanim0920@gmail.com>
2021-10-27 01:22:01 -0700

    partially implemented the matching code from this part https://www.youtube.com/watch?v=U2-JVhRLQTw&list=PL4vbr3u7UKWqwQlvwvgNcgDL1p_3hcNn2&index=6

--- grid.gd
+++ grid.gd
@@ -40,11 +40,26 @@ func spawn_tiles():
 	for i in columns:
 		for j in rows:
 			var rand = floor(rand_range(0, tile_prototypes.size()));
+			
+			#match checking code here
+			#would be nice if we could associate the color enum with the tile prototypes
+			#switch to picking a different type at random if rand results in a match
+			
 			var tileInstance = tile_prototypes[rand].instance();
 			var scale = tile_size_in_pixels / 128; #may want to check the size of the sprite rather than hardcoding 128
 			tileInstance.scale = Vector2(scale, scale);
 			add_child(tileInstance);
 			tileInstance.position = grid_to_pixel(i, j);
+			all_tiles[i][j] = tileInstance;
+			
+func check_for_match(column, row, color):
+	if column > 1:
+		if all_tiles[column - 1][row].Color == color && all_tiles[column - 2][row].Color == color:
+			return true;
+				
+	if row > 1:
+		if all_tiles[column][row - 1].Color == color && all_tiles[column][row - 2].Color == color:
+			return true;
 	
 func grid_to_pixel(column, row):
 	var x = horizontal_margin + (tile_size_in_pixels/2) + tile_size_in_pixels * column;
