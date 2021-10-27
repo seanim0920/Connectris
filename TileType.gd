e0eb209d905fbdc6015021045692a618bd947800
sean <seanim0920@gmail.com>
2021-10-27 15:01:31 -0700

    board now fills without a match. using the algorithm from this tutorial https://www.youtube.com/watch?v=U2-JVhRLQTw&list=PL4vbr3u7UKWqwQlvwvgNcgDL1p_3hcNn2&index=6&ab_channel=MisterTaftCreates tiletype's enum order had to be switched to match the order of the files in the tiletypes directory, otherwise matches won't accurately be detected. in the future we may want to do away with the enum and base the logic exclusively on the contents of the tiletypes folder to avoid little bugs like that. pink and yellow's enums had to be switched to match the order of the files in the tiletypes directory,

--- TileType.gd
+++ TileType.gd
@@ -1,4 +1,4 @@
 class_name TileType
 extends Reference
 
-enum COLOR {blue, green, orange, yellow, pink}
+enum COLOR {blue, green, orange, pink, yellow}
--- grid.gd
+++ grid.gd
@@ -38,12 +38,21 @@ func make_new_2d_array():
 	
 func spawn_tiles():
 	for i in columns:
-		for j in rows:
-			var rand = floor(rand_range(0, tile_prototypes.size()));
-			
+		for j in rows:			
 			#match checking code here
 			#would be nice if we could associate the color enum with the tile prototypes
 			#switch to picking a different type at random if rand results in a match
+			var availableColors = TileType.COLOR.values();
+			var rand = randi() % availableColors.size();
+			
+			while(check_for_match(i, j, availableColors[rand])):
+				availableColors.remove(availableColors[rand]);
+				rand = randi() % availableColors.size();
+				#instead of moving to the next tile type (might not be random enough)
+				#make a new array of colors and subtract the chosen color
+				#pick a random color from the leftovers
+				
+				#in the future we may want to do away with the enum and base the logic exclusively on the contents of the tiletypes folder.
 			
 			var tileInstance = tile_prototypes[rand].instance();
 			var scale = tile_size_in_pixels / 128; #may want to check the size of the sprite rather than hardcoding 128
@@ -54,11 +63,14 @@ func spawn_tiles():
 			
 func check_for_match(column, row, color):
 	if column > 1:
-		if all_tiles[column - 1][row].Color == color && all_tiles[column - 2][row].Color == color:
+		#print("checking match at ", column, ", ", row);
+		#print("current color is ", color);
+		#print("comparing to the left neighbor which is ", all_tiles[column - 1][row].color);
+		if all_tiles[column - 1][row].color == color && all_tiles[column - 2][row].color == color:
 			return true;
 				
 	if row > 1:
-		if all_tiles[column][row - 1].Color == color && all_tiles[column][row - 2].Color == color:
+		if all_tiles[column][row - 1].color == color && all_tiles[column][row - 2].color == color:
 			return true;
 	
 func grid_to_pixel(column, row):
